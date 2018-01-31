a1e73095d424ced57899f91d5e03013c00817bf4
Trever Andersen <trever.r.andersen@gmail.com>
2018-01-31 00:05:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach(function (d) {
+    console.log(d.name)
+  });
+}
+
+function logDriversByHometown(drivers, location) {
+  drivers.forEach(function (d) {
+    if (d.hometown === location) {
+      console.log(d.name);
+    }
+  });
+}
+
+function driversByRevenue(drivers) {
+  drivers.sort(function (a, b) {
+    return a.revenue - b.revenue;
+  });
+  return drivers;
+}
+
+function driversByName(drivers) {
+  
+}
