09439510e87ab63f9c05c9fc22d95b1d59649137
Trever Andersen <trever.r.andersen@gmail.com>
2018-02-16 18:47:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,13 +12,7 @@ function logDriversByHometown(drivers, location) {
     }
   });
 }
-const drivers = [
-  { name: 'Bobby',   hometown: 'Pittsburgh',  revenue: 3000 },
-  { name: 'Sally',   hometown: 'New York',    revenue: 2000 },
-  { name: 'Sammy',   hometown: 'Pittsburgh',  revenue: 2500 },
-  { name: 'Annette', hometown: 'Los Angeles', revenue: 6000 },
-  { name: 'Bobby',   hometown: 'Tampa Bay',   revenue: 5000 }
-];
+
 function driversByRevenue(drivers) {
   arr = drivers.revenue
   a.sort(function (a, b) {
