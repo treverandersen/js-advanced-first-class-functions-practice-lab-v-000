0d82b7ef696f6da7257fa37b892ad43b1ad77f5a
Trever Andersen <trever.r.andersen@gmail.com>
2018-02-16 20:38:28 +0000

    Done.

--- index.js
+++ index.js
@@ -15,11 +15,18 @@ function logDriversByHometown(drivers, location) {
 
 function driversByRevenue(drivers) {
   drivers.sort(function (a, b) {
-    return a.revenue - b.revenue;
+    return a.localeCompare(b);
   });
-  return drivers;
 }
 
 function driversByName(drivers) {
-  
+
+}
+
+function totalRevenue() {
+
+}
+
+function averageRevenue() {
+
 }
